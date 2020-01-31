ef9cac3e7b0a5a83e9c36ab1a19f3d19c7cd01c2
fadimata tillman <fadi.tillman@gmail.com>
2020-01-31 23:07:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ def find_the_cheese(array)
   # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-   if array.include?("")
+   if array.include?("cheddar")
   "cheddar"
 else nil
   end
