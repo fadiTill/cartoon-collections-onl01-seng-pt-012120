8e9b2dc76cfa403c6c7fb6be1e0a5afa3af48d11
fadimata tillman <fadi.tillman@gmail.com>
2020-01-31 23:08:55 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves(array)
+array.each_with_index  { |name,index|
+puts "#{index+1}. #{name}"
+  #array[element] = index
+ }
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(herro)
+  herro.map! {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
   # Your code here
+
+def long_planeteer_calls(array)# code an argument here
+  array.map {|elements|}
+  if array.length == 4
+     true
+  else false
+end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
+  # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+   if array.include?("cheddar")
+  "cheddar"
+else nil
+  end
 end
