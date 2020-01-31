74984f9b60ab13f9c10d05e51797c352a6436b89
fadimata tillman <fadi.tillman@gmail.com>
2020-01-31 22:54:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,8 +26,6 @@ def find_the_cheese(cheese_types)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
    if cheese_types.include?("cheddar")
-  puts   "cheddar"
-
-  
-
+  "cheddar"
+  end
 end
